28d5a3bb42e782d9bc914a4a99baae1f89726619
Isbah Kassam <isbahashif06@gmail.com>
2026-03-06 12:10:49 +0300

    initiating https mongo handshake test

--- server.js
+++ server.js
@@ -1,28 +1,40 @@
-const express = require ('express');
-const mongoose = require ('mongoose');
-const cors = require ('cors');
-const bodyParser = require ('body-parser');
-const dotenv = require ('dotenv');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
 
 dotenv.config();
-const app = express()
+const app = express();
 
 const hostName = process.env.HOSTNAME || localhost;
 const port = process.env.PORT || 3000;
 
+const connectDB = async () => {
+  try {
+    const uri = process.env.MONGODB_URI;
+    if (!uri) {
+      throw new Error("check URI");
+    }
+    await mongoose.connect(uri, {
+      serverSelectionTimeoutMS: 5000,
+      maxPoolSize: 10,
+    });
+    
+    console.log("Mongo Atlas successfully connected");
+  } catch (err) {
+    console.error("Mongo Atlas connection error:", err);
+    process.exit(1);
+  }
+};
 
+connectDB();
 
-app.get("/" , (req, res)=>{
-res.send("This is the Cyan API")
+app.get("/", (req, res) => {
+  res.send("This is the Cyan API");
+});
 
-
-
-})
-
-app.listen(port, ()=>{
-    console.log (`Cyan server is running at http://${hostName}:${port}/`)
-    console.log ("working!")
-
-
-
-})
+app.listen(port, () => {
+  console.log(`Cyan server is running at http://${hostName}:${port}/`);
+  console.log("working!");
+});
